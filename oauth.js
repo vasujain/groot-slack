50f3c069f44716fd7d5dbb37b06a5dab6ba0c04c
Vasu Jain <vasu.jain@live.in>
2016-12-16 17:13:09 -0800

    auth call with code

--- oauth.js
+++ oauth.js
@@ -4,15 +4,6 @@
  */
 
 // Documentation @ https://api.slack.com/docs/slack-button
-
-// if the oAuth request was accepted, the URL will contain a temporary code in a GET code parameter , 
-// as well as the state you provided in the previous step in a state parameter
-// state -- unique string to be passed back upon completion
-// States should match here.
-
-
-// exchange the code for an access token using the oauth.access method. 
-
 // sample JSON response for oAuth call:
 /*
     {
@@ -35,10 +26,13 @@
 
 var BotConfig = require('./config.json');
 
-slackAuthUrl();
+var authCode = extractSlackCode("code");
+slackTokenIssuerCall(authCode);
+
+// This call is already being made by Slack Button
 function slackAuthUrl() {
     var authUrl = BotConfig.slack.oauth_authorize_url 
-      + "?client_id=" + BotConfig.slack.oauth_authorize_url.client_id 
+      + "?client_id=" + BotConfig.slack.client_id 
       + "&scope=" + BotConfig.slack.scope 
       + "&redirect_uri=" + BotConfig.slack.redirect_uri 
       + "&team=" + BotConfig.slack.team 
@@ -46,30 +40,45 @@ function slackAuthUrl() {
     window.location = authUrl;
 }
 
-
-function extractSlackCode() {
-    //https://groot-slack.herokuapp.com/oauth.js?code=XXXX&state=
-
+function extractSlackCode(parameterName) {
+   //https://APP_NAME.herokuapp.com/oauth.js?code=XXXX&state=
+    var result = null, tmp = [];
+    location.search.substr(1).split("&").forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) 
+                result = decodeURIComponent(tmp[1]);
+    });
+    return result;
 }
 
-function slackTokenIssuerCall() {
-    // Request:
-    // https://slack.com/api/oauth.access?client_id=XXXX&client_secret=XXXX&code=XXXX&pretty=1
+function slackTokenIssuerCall(auth_code) {
+    // Request: https://slack.com/api/oauth.access?client_id=XXXX&client_secret=XXXX&code=XXXX&pretty=1
     
-    // Response: 
-    /*
-    
-    {
-        "ok": true,
-        "access_token": "xoxp-XXXX",
-        "scope": "identify,channels:read,chat:write:bot",
-        "user_id": "U1XLXXXXLXXXXNQ",
-        "team_name": "XXXX",
-        "team_id": "T1XXXXXLSXXXX27NU"
-    }
-    
-    
-    */
+    var http = require("https");
+    var path = "/api/oauth.access" 
+        + "?client_id=" + BotConfig.slack.client_id
+        + "&client_secret=" + BotConfig.slack.client_secret 
+        + "&code=" + auth_code;
+
+    var options = {
+      "method": "GET",
+      "hostname": "slack.com",
+      "port": null,
+      "path": path
+    };
+
+    var req = http.request(options, function (res) {
+      var chunks = [];
+
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on("end", function () {
+        var body = Buffer.concat(chunks);
+        console.log(body.toString());
+      });
+    });
+
+    req.end();
 }
-
-
